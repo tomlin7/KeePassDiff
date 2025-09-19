d5bc91bc16e06bf721e8d301c9d6fb4e1b538528
Serrindipity <jon.quitoriano@gmail.com>
2025-09-19 15:50:56 -0700

    sanitize path

--- KeePassDiff/launch.py
+++ KeePassDiff/launch.py
@@ -1,11 +1,14 @@
 import os
 import subprocess
+from shlex import quote
 
 
 def launch():
     path = os.path.dirname(os.path.abspath(__file__))
+    app_path = f"{path}/app.py"
 
-    subprocess.run(f"streamlit run {path}/app.py", shell=True)
+    app_path = quote(app_path)
+    subprocess.run(args=["streamlit", "run", app_path], shell=True)
 
 
 if __name__ == "__main__":
