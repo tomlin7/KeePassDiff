6e6d72df1d7d3d5af6740aa40a2ac8ad44a8b55e
Billy <billydevbusiness@gmail.com>
2025-09-20 12:39:21 +0530

    fix: Remove shlex

--- KeePassDiff/launch.py
+++ KeePassDiff/launch.py
@@ -1,14 +1,11 @@
 import os
 import subprocess
-from shlex import quote
 
 
 def launch():
     path = os.path.dirname(os.path.abspath(__file__))
-    app_path = f"{path}/app.py"
-
-    app_path = quote(app_path)
-    subprocess.run(args=["streamlit", "run", app_path], shell=True)
+    app_path = os.path.join(path, "app.py")
+    subprocess.run(["streamlit", "run", app_path], shell=True)
 
 
 if __name__ == "__main__":
